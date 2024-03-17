af8158545a17d62afa50d9e2220a78d5c10fc5cc
Elijah Wines <wines.elijah@gmail.com>
2024-03-17 13:07:48 -0400

    create api route

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db');
+const { readdirSync } = require('fs')
 const app = express();
 
 require('dotenv').config()
@@ -11,9 +12,8 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+// routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
     db()
