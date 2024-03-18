ca75b5a6bb8b6f44457c6585dfb7a0edb225fd9a
Elijah Wines <wines.elijah@gmail.com>
2024-03-18 19:18:40 -0400

    testing

---
+++ backend/controllers/expense.js
@@ -0,0 +1,13 @@
+const ExpenseSchema = require('../models/expenseModel')
+
+exports.addExpense = async (req, res) => {
+
+}
+
+exports.getExpenses = async (req, res) => {
+    
+}
+
+exports.deleteExpense = async (req, res) => {
+    
+}
