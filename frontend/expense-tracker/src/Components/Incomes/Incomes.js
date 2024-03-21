1ad7c9d17b7c2bb9660e27379b3ab2c7ce95c6c0
Elijah Wines <wines.elijah@gmail.com>
2024-03-21 18:22:54 -0400

    created Form and Button Component

--- frontend/expense-tracker/src/Components/Incomes/Incomes.js
+++ frontend/expense-tracker/src/Components/Incomes/Incomes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/layouts'
 import { useGlobalContext } from '../../Context/globalContext'
+import Form from '../Form/Form'
 
 const Incomes = () => {
 
@@ -13,7 +14,7 @@ const Incomes = () => {
         <h2>Incomes</h2>
         <div className='income-content'>
           <div className='form-container'>
-
+            <Form />
           </div>
           <div className='incomes'>
 
