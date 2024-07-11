98e448d0c7495dd1e2dfe0b35fb9e2367de520ce
Elijah Wines <wines.elijah@gmail.com>
2024-07-11 17:40:39 -0400

    updated base_url

--- frontend/expense-tracker/src/Context/globalContext.js
+++ frontend/expense-tracker/src/Context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api/v1'
+const BASE_URL = 'https://expense-tracker-backend-2ug1.onrender.com/api/v1'
 
 const GlobalContext = React.createContext()
 
